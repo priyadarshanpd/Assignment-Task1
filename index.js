ed4a0fbfd8517054b201db98565d95518c4a3bd3
priyadarshanpd <priyadarshan28797@gmail.com>
2021-06-03 16:01:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   //change#1
-  //change#2
-  //change#3
   const msg = 'Hello World!\n'
   res.end(msg);
 });
